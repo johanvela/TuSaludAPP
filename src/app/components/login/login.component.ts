6469faeabdf0bd3db4e5a41464ca4b48c8a9c303
Diego Gatica <diegogatica@Diegos-MacBook-Pro.local>
2024-12-09 21:14:05 -0500

    Correcion redireccion a register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,24 +14,6 @@ import { LoginResponse } from '../../models/user.model';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-/*
-export class LoginComponent {
-  credentials = { correo: '', contrasena: '' };
-
-  constructor(private router: Router) {}
-
-  onSubmit() {
-    console.log('Credenciales ingresadas:', this.credentials);
-    // Aquí iría la lógica para autenticar al usuario con un AuthService
-    if (this.credentials.correo === 'test@example.com' && this.credentials.contrasena === '12345') {
-      alert('Login exitoso');
-      this.router.navigate(['/dashboard']); // Redirige al dashboard esto va ser el home a la larga,  si el login es exitoso
-    } else {
-      alert('Nombre de usurio o contraseña incorrecta');
-    }
-  }
-}
-*/
 
 export class LoginComponent {
   credentials: Credentials = { correo: '', contrasena: '' };
@@ -57,5 +39,10 @@ export class LoginComponent {
         console.log('Proceso de login completo');
       },
     });
+  }
+
+  navigateToRegister() {
+    console.log('Navegando a /register...');
+    this.router.navigate(['/register']);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { LoginComponent } from './app/components/login/login.component';
 import { routes } from  './app/app.routes';
+import { AppComponent } from './app/app.component';
 
 
-bootstrapApplication(LoginComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()],
 }).catch((err) => console.error(err));
